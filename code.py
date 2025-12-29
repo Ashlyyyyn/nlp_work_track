87e4b4aff1f72b633ef8dc2766b03eb62f879852
Ashlyyyyn <3078164713@qq.com>
2025-12-29 23:59:42 +0800

    Delete code.py

--- code.py
+++
@@ -1,5 +0,0 @@
-﻿from matplotlib import font_manager
-
-# Force Matplotlib to regenerate its cached font list.
-font_manager._load_fontmanager(try_read_cache=False)
-print("Matplotlib font cache rebuilt.")
